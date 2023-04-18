b22d46e7e2d57f00405e7ad1e2134b1f4ef3acee
Zak Mayfield <zakmayfield@gmail.com>
2023-04-18 11:57:51 -0400

    example challenge added

---
+++ gpt/course/2_oop/2_classes/exercises/challenge_2.py
@@ -0,0 +1,38 @@
+"""
+You are tasked with modeling a simple library system. The library has books, and each book has a title, author, genre, and availability status (i.e., whether it is currently borrowed or not).
+
+Requirements:
+
+1.) Create a class called Book that represents a book in the library. The Book class should have the following attributes:
+
+* `title`: a string representing the title of the book
+* `author`: a string representing the author of the book
+* `genre`: a string representing the genre of the book
+* `available`: a boolean value representing the availability status of the book (True if available, False if borrowed)
+
+2.) Define a method display_info() within the Book class that displays the information of the book, including its title, author, genre, and availability status.
+
+3.) Instantiate three Book objects with different attributes to represent three books in the library.
+
+Steps:
+
+1.) Create a new Python file and define the Book class with the attributes and method as described in the requirements.
+
+2.) Instantiate three Book objects with different attributes to represent three books in the library.
+
+3.) Call the display_info() method on each Book object to display the information of the books.
+
+4.) Modify the availability status of one of the books to reflect that it has been borrowed.
+
+5.) Call the display_info() method on the modified Book object to verify the change in availability status.
+Test your implementation by running the Python file and checking the output for correctness.
+
+example output
+
+```
+title: "The Catcher in the Rye"
+author: "J.D. Salinger"
+genre: "Fiction"
+available: True | False
+```
+"""
---
+++ gpt/course/2_oop/2_classes/exercises/challenges.md
@@ -0,0 +1,32 @@
+# Lesson 5: Practice Exercises
+
+## Overview
+
+In this lesson, you will have the opportunity to apply the concepts you've learned about object-oriented programming (OOP) in Python through hands-on exercises. You will be tasked with creating classes, defining attributes and methods, instantiating objects, and using objects to perform actions based on real-world scenarios.
+
+## Objectives
+
+By the end of this lesson, you should be able to:
+
+- Apply the concepts of OOP to solve practical problems
+- Create classes and objects to model real-world scenarios
+- Define attributes and methods within classes
+- Instantiate objects with different attributes
+- Use objects to perform actions and modify object state
+
+# Lesson Content
+
+## Practice Exercise 1: Creating a Class for a Real-World Scenario
+
+- Introduce a real-world scenario, such as modeling a library or a car rental system
+- Discuss the requirements and functionalities of the scenario
+
+## Practice Exercise 2: Using Objects to Perform Actions
+
+- Provide a set of tasks that involve using objects to perform actions
+- Task examples could include borrowing a book from a library, renting a car, or performing operations on a bank account
+
+## Practice Exercise 3: Applying Inheritance and Encapsulation
+
+- Introduce the concepts of inheritance and encapsulation
+- Provide a scenario that can benefit from using inheritance and encapsulation, such as modeling different types of vehicles or animals
